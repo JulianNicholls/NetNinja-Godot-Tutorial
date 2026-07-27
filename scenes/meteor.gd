b7dc5c06aca1c25610e99c798d984a32b073db7e
Julian Nicholls <juliannicholls29@gmail.com>
2026-07-27 11:49:04 +0100

    Add borders and collisions, using signals.

---
+++ scenes/meteor.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+func _on_body_entered(body: Node2D) -> void:
+	print('Bang!') # Replace with function body.
+	print(body)
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed: int = 300
 
@@ -15,7 +15,8 @@ func _process(delta: float) -> void:
 	#print(delta)
 	
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
 
 #func _input(event: InputEvent) -> void:
 	#if event is InputEventKey and event.is_pressed():
