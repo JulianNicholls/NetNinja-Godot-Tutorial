6d353c1378b841345733fb109c47146f12cb27cd
Julian Nicholls <juliannicholls29@gmail.com>
2026-07-27 15:16:54 +0100

    Update the meteor movement and random texture, and set the collision layers.

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,4 +1,13 @@
 extends Node2D
 
-func _on_timer_timeout() -> void:
-	print('Meteor')
+# 1. Load the meteor scene
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
+
+func _on_meteor_timer_timeout() -> void:
+	# 2. Create an instance
+	var meteor	= meteor_scene.instantiate()
+	
+	meteor.position = Vector2(100, 100)
+	
+	# 3. Attach the scene to the tree
+	$Meteors.add_child(meteor)
--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,5 +1,30 @@
 extends Area2D
 
+var speed: int;
+var rot: float
+var dirX: float
+
+func _ready() -> void:
+	var rng := RandomNumberGenerator.new()		
+
+	var num = rng.randi_range(1, 4)
+	var path: String = "res://assets/PNG/Meteors/meteorBrown_big" + str(num) + ".png"
+	$MeteorImage.texture = load(path)
+	
+	var width := get_viewport_rect().size[0]
+	var randomX = rng.randi_range(0, width)
+	var randomY = rng.randi_range(-150, -50);
+
+	speed = rng.randi_range(200, 500)
+	dirX = rng.randi_range(-1, 1)
+	rot = rng.randf_range(-2, 2)
+	
+	position = Vector2(randomX, randomY)
+	 
+func _process(delta: float) -> void:
+	position += Vector2(dirX, 1.0) * speed * delta
+	rotation += rot * delta
+		
 func _on_body_entered(body: Node2D) -> void:
 	print('Bang!') # Replace with function body.
 	print(body)
